36d43595bf86aa5c447b38cb04a39baf81ca2584
Cole K <cole-k@users.noreply.github.com>
2023-03-15 11:50:49 -0700

    hashmap optimization

--- src/lambda_destructive_rewrite.rs
+++ src/lambda_destructive_rewrite.rs
@@ -1,5 +1,6 @@
 use egg::{rewrite as rw, *};
 use fxhash::FxHashSet as HashSet;
+use fxhash::FxHashMap as HashMap;
 
 define_language! {
     enum Lambda {
@@ -267,75 +268,106 @@ impl Applier<Lambda, LambdaAnalysis> for DestructiveRewrite {
         rule_name: Symbol,
     ) -> Vec<Id> {
         let ids = self.add_pattern.apply_one(egraph, eclass, subst, searcher_ast, rule_name);
-        let rec_expr_id: Id = (self.original_pattern.ast.as_ref().len() - 1).into();
-        // Remove any nodes that match the pattern
-        let filtered_nodes = egraph[eclass].nodes
-            .clone()
-            .into_iter()
-            .filter(|node| {
-                let res = match_enode(egraph, &self.original_pattern.ast, &rec_expr_id, subst, node, HashSet::default());
-                if res {
-                    // println!("{} filtering node {:?}", rule_name, node)
-                }
-                !res
-            })
-            .collect();
-        egraph[eclass].nodes = filtered_nodes;
+        prune_enodes_matching(egraph, &self.original_pattern.ast, subst, &eclass);
         ids
     }
 }
 
-// inefficient: lots of cloning of seen. Unsure how to avoid cloning it, will require some thought.
-fn match_enode(egraph: &egg::EGraph<Lambda, LambdaAnalysis>, rec_expr: &RecExpr<ENodeOrVar<Lambda>>, rec_expr_id: &Id, subst: &Subst, enode: &Lambda, seen: HashSet<(Id, Id)>) -> bool {
+/// Removes enodes matching the rec_expr from the egraph.
+///
+/// I think that we could do slightly better than a HashMap by having a mutable
+/// RecExpr and storing which Ids we've visited on the nodes, but the difference
+/// between passing around clones of a HashMap/HashSet everywhere and using a
+/// single mutable HashMap is minimal in my testing (0.2s for a test taking 9s -
+/// although this was just a single test).
+fn prune_enodes_matching(egraph: &mut egg::EGraph<Lambda, LambdaAnalysis>, rec_expr: &RecExpr<ENodeOrVar<Lambda>>, subst: &Subst, eclass: &Id) {
+    let mut memo = HashMap::default();
+    let rec_expr_id: Id = (rec_expr.as_ref().len() - 1).into();
+    // Handles cycles - if we get back here then it matches.
+    memo.insert((rec_expr_id, *eclass), true);
+    let filtered_nodes = egraph[*eclass].nodes
+        .clone()
+        .into_iter()
+        .filter(|node| {
+            let res = match_enode(egraph, &rec_expr, &rec_expr_id, subst, node,  &mut memo);
+            if res {
+                // println!("{} filtering node {:?}", rule_name, node)
+            }
+            !res
+        })
+        .collect();
+    egraph[*eclass].nodes = filtered_nodes;
+}
+
+/// This function recursively traverses the rec_expr and enode in lock step. If
+/// they have matching constants, then we can simply check their equality. Most
+/// of the cases, however, come from recursively checking the contained rec_expr
+/// nodes against contained eclasses.
+fn match_enode(egraph: &egg::EGraph<Lambda, LambdaAnalysis>, rec_expr: &RecExpr<ENodeOrVar<Lambda>>, rec_expr_id: &Id, subst: &Subst, enode: &Lambda, memo: &mut HashMap<(Id, Id), bool>) -> bool {
     match &rec_expr[*rec_expr_id] {
         ENodeOrVar::ENode(n) => {
             match (n, enode) {
+                // First base cases are when leaves of the expressions match
                 (Lambda::Bool(b_re), Lambda::Bool(b)) => b_re == b,
                 (Lambda::Num(n_re), Lambda::Num(n)) => n_re == n,
                 (Lambda::Symbol(s_re), Lambda::Symbol(s)) => s_re == s,
+                // Recursive cases
                 (Lambda::Var(v_re), Lambda::Var(v)) =>
-                    any_enode_in_eclass_matches(egraph, rec_expr, v_re, subst, v, seen),
+                    any_enode_in_eclass_matches(egraph, rec_expr, v_re, subst, v, memo),
                 (Lambda::Add([n1_re, n2_re]), Lambda::Add([n1, n2])) =>
-                    any_enode_in_eclass_matches(egraph, rec_expr, n1_re, subst, n1, seen.clone())
-                    && any_enode_in_eclass_matches(egraph, rec_expr, n2_re, subst, n2, seen),
+                    any_enode_in_eclass_matches(egraph, rec_expr, n1_re, subst, n1, memo)
+                    && any_enode_in_eclass_matches(egraph, rec_expr, n2_re, subst, n2, memo),
                 (Lambda::App([e1_re, e2_re]), Lambda::App([e1, e2])) =>
-                    any_enode_in_eclass_matches(egraph, rec_expr, e1_re, subst, e1, seen.clone())
-                    && any_enode_in_eclass_matches(egraph, rec_expr, e2_re, subst, e2, seen),
+                    any_enode_in_eclass_matches(egraph, rec_expr, e1_re, subst, e1, memo)
+                    && any_enode_in_eclass_matches(egraph, rec_expr, e2_re, subst, e2, memo),
                 (Lambda::Lambda([x_re, body_re]), Lambda::Lambda([x, body])) =>
-                    any_enode_in_eclass_matches(egraph, rec_expr, x_re, subst, x, seen.clone())
-                    && any_enode_in_eclass_matches(egraph, rec_expr, body_re, subst, body, seen),
+                    any_enode_in_eclass_matches(egraph, rec_expr, x_re, subst, x, memo)
+                    && any_enode_in_eclass_matches(egraph, rec_expr, body_re, subst, body, memo),
                 (Lambda::Let([x_re, v_re, e_re]), Lambda::Let([x, v, e])) =>
-                    any_enode_in_eclass_matches(egraph, rec_expr, x_re, subst, x, seen.clone())
-                    && any_enode_in_eclass_matches(egraph, rec_expr, v_re, subst, v, seen.clone())
-                    && any_enode_in_eclass_matches(egraph, rec_expr, e_re, subst, e, seen),
+                    any_enode_in_eclass_matches(egraph, rec_expr, x_re, subst, x, memo)
+                    && any_enode_in_eclass_matches(egraph, rec_expr, v_re, subst, v, memo)
+                    && any_enode_in_eclass_matches(egraph, rec_expr, e_re, subst, e, memo),
                 (Lambda::Fix([e1_re, e2_re]), Lambda::Fix([e1, e2])) =>
-                    any_enode_in_eclass_matches(egraph, rec_expr, e1_re, subst, e1, seen.clone())
-                    && any_enode_in_eclass_matches(egraph, rec_expr, e2_re, subst, e2, seen),
+                    any_enode_in_eclass_matches(egraph, rec_expr, e1_re, subst, e1, memo)
+                    && any_enode_in_eclass_matches(egraph, rec_expr, e2_re, subst, e2, memo),
                 (Lambda::If([b_re, e1_re, e2_re]), Lambda::If([b, e1, e2])) =>
-                    any_enode_in_eclass_matches(egraph, rec_expr, b_re, subst, b, seen.clone())
-                    && any_enode_in_eclass_matches(egraph, rec_expr, e1_re, subst, e1, seen.clone())
-                    && any_enode_in_eclass_matches(egraph, rec_expr, e2_re, subst, e2, seen),
+                    any_enode_in_eclass_matches(egraph, rec_expr, b_re, subst, b, memo)
+                    && any_enode_in_eclass_matches(egraph, rec_expr, e1_re, subst, e1, memo)
+                    && any_enode_in_eclass_matches(egraph, rec_expr, e2_re, subst, e2, memo),
                 _ => false
             }
         }
+        // I think this is incomparable - an enode is not an eclass. Perhaps
+        // they are equal if the enode is in the eclass? I kind of don't think
+        // so.
         ENodeOrVar::Var(_) => false,
     }
 }
 
-fn any_enode_in_eclass_matches(egraph: &egg::EGraph<Lambda, LambdaAnalysis>, rec_expr: &RecExpr<ENodeOrVar<Lambda>>, rec_expr_id: &Id, subst: &Subst, eclass: &Id, seen: HashSet<(Id, Id)>) -> bool {
-    if seen.contains(&(*rec_expr_id, *eclass)) {
-        true
-    } else {
+/// In this case, we have a concrete AST node (ENodeOrVar::EnNode) or Var
+/// (ENodeOrVar::Var) in the rec_expr that we want to compare to an entire
+/// eclass. Comparing a Var to an eclass is a base case - we just check to see
+/// if they're the same. Otherwise, we need to check if there is any enode in
+/// the class that we can match with the concrete AST node.
+fn any_enode_in_eclass_matches(egraph: &egg::EGraph<Lambda, LambdaAnalysis>, rec_expr: &RecExpr<ENodeOrVar<Lambda>>, rec_expr_id: &Id, subst: &Subst, eclass: &Id, memo: &mut HashMap<(Id, Id), bool>) -> bool {
+    if let Some(res) = memo.get(&(*rec_expr_id, *eclass)) {
+        return *res
+    }
+    let res = {
+        // This is the second and last base case (aside from cycles) where we can
+        // conclude a pattern matches.
         if let ENodeOrVar::Var(v) = rec_expr[*rec_expr_id] {
             return subst[v] == *eclass;
         }
-        let mut new_seen = seen.clone();
-        new_seen.insert((*rec_expr_id, *eclass));
-        egraph[*eclass].iter().any(|node| match_enode(egraph, rec_expr, &rec_expr_id, subst, node, new_seen.clone()))
-    }
+        // If we cycle back to this node, then the pattern matches.
+        memo.insert((*rec_expr_id, *eclass), true);
+        egraph[*eclass].iter().any(|node| match_enode(egraph, rec_expr, &rec_expr_id, subst, node, memo))
+    };
+    // Update the memo since we only set it to 'true' temporarily to handle cycles.
+    memo.insert((*rec_expr_id, *eclass), res);
+    res
 }
 
-// TODO: make destructive
 struct CaptureAvoid {
     original_pattern: Pattern<Lambda>,
     fresh: Var,
@@ -367,21 +399,7 @@ impl Applier<Lambda, LambdaAnalysis> for CaptureAvoid {
             self.if_not_free
                 .apply_one(egraph, eclass, subst, searcher_ast, rule_name)
         };
-        // TODO make own function
-        let rec_expr_id: Id = (self.original_pattern.ast.as_ref().len() - 1).into();
-        // Remove any nodes that match the pattern
-        let filtered_nodes = egraph[eclass].nodes
-            .clone()
-            .into_iter()
-            .filter(|node| {
-                let res = match_enode(egraph, &self.original_pattern.ast, &rec_expr_id, subst, node, HashSet::default());
-                if res {
-                    // println!("{} filtering node {:?}", rule_name, node)
-                }
-                !res
-            })
-            .collect();
-        egraph[eclass].nodes = filtered_nodes;
+        prune_enodes_matching(egraph, &self.original_pattern.ast, subst, &eclass);
         ids
     }
 }
