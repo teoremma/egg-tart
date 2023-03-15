344e013cc3d26fd3cf260e46ed9aee97b65e99b4
Cole K <cole-k@users.noreply.github.com>
2023-03-15 02:34:34 -0700

    destructive rewrites woo

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod reps;
 // use reps::named::Named;
 // use reps::named::Named::*;
 use reps::named::*;
+mod lambda_destructive_rewrite;
 
 fn main() {
     let mut lhs = lam(lam(app(app(var(4), var(2)), lam(app(var(1), var(3))))));
