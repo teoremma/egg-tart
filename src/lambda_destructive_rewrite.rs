eced8f6148c6f4a929f7c2f06e87938f9e0f2ea1
Cole K <cole-k@users.noreply.github.com>
2023-04-04 14:44:51 -0700

    add graphical output

--- src/lambda_destructive_rewrite.rs
+++ src/lambda_destructive_rewrite.rs
@@ -751,6 +751,40 @@ egg::test_fn! {
 }
 
 #[test]
+fn dr_simple_example() {
+    // let start = "(app (lam comp (app (lam inc (app (app (var comp) (var inc)) (var inc) ))
+    //                                  (lam y (+ (var y) 1))))
+    //                   (lam f (lam g (lam x (app (var f)
+    //                                             (app (var g) (var x)))))))".parse().unwrap();
+    // let goal = "(lam ?x (+ (var ?x) 2))".parse().unwrap();
+
+    // // Simple 1
+    // let start = "(app (lam x (+ (var x) (var x))) 2)".parse().unwrap();
+    // let goal = "4".parse().unwrap();
+
+    // // Id 1
+    // let start = "(app (lam x (var x)) (var x))".parse().unwrap();
+    // let goal = "(var x)".parse().unwrap();
+
+    // Id 1
+    let start = "(app (lam x (app (var x) (app (var y) (var z)))) 1)".parse().unwrap();
+    let goal = "(app 1 (app (var y) (var z)))".parse().unwrap();
+
+    let mut runner: Runner<Lambda, LambdaAnalysis> = Runner::default();
+    runner = runner.with_expr(&start);
+    let id = runner.egraph.find(*runner.roots.last().unwrap());
+    let before_path = "dots/dr_simple_before.png";
+    runner.egraph.dot().to_png(before_path).unwrap();
+
+    runner = runner.run(&rules());
+    println!("{report}", report = runner.report());
+    runner.egraph.check_goals(id, &[goal]);
+
+    let after_path = "dots/dr_simple_after.png";
+    runner.egraph.dot().to_png(after_path).unwrap();
+}
+
+#[test]
 fn lambda_dr_fib_range() {
     let range = 0..30;
     for n in range {
